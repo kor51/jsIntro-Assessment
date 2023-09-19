bc371b1d90a4d2a5149b2d8b298fa7954e0c86d9
Korie <theultimateveemo@gmail.com>
2023-09-19 00:03:20 -0400

    initial commit

---
+++ jsIntro.js
@@ -0,0 +1,7 @@
+    var firstName = "Korie";
+    var lastName = "Raber";
+    var age = 17;
+
+    alert("My name is " + firstName + " " + lastName);
+    alert("I am " + age + " years old");
+    console.log("This class is pretty fun!");
